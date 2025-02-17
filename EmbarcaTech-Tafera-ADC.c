37ac28532920db2ffd9375d9629355ed77ecf996
Luis Eduardo F. S. Pereira <eduardo38sao3@live.com>
2025-02-17 17:29:37 -0300

    Adicionado comentário para explicação do código.

--- EmbarcaTech-Tafera-ADC.c
+++ EmbarcaTech-Tafera-ADC.c
@@ -9,7 +9,7 @@
 #include "bibliotecas/font.h"
 #include "bibliotecas/ssd1306.h"
 
-//Leds
+//Declaração dos pinos dos leds e estados dos mesmos
 const uint8_t led_red_pino = 13;
 const uint8_t led_blue_pino = 12;
 const uint8_t led_green_pino = 11;
@@ -18,38 +18,40 @@ volatile bool estado_blue = false;
 volatile bool estado_green = false;
 volatile bool estado_trava_led = false;
 
-//Botões
+//Declaração dos pinos dos botões da placa
 const uint8_t btn_a = 5;
 const uint8_t btn_b = 6;
 const uint8_t btn_j = 22;
 
-//Eixos joystick_pino_Y
+//Declaração do spinos dos do joystick
 const uint8_t joystick_pino_X = 26;
 const uint8_t joystick_pino_Y = 27;
 
-//Display
+//Declaração dos parâmentros e pinos do display SSD1306
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
 #define endereco 0x3C
 
-//Variáveis blobais
+//Declaração de variáveis blobais
 volatile uint32_t ultimo_tempo = 0;
 volatile bool cor = true;
 
-//Protótipos
+//Protótipos das funções
 void iniciar_pinos();
 void gpio_irq_handler(uint gpio, uint32_t events);
 
-int main()
-{
+int main(){
+    //Inicializando os bibliotecas e funções.
     stdio_init_all();
     iniciar_pinos();
 
+    //Declaração das interrupções
     gpio_set_irq_enabled_with_callback(btn_a, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     gpio_set_irq_enabled_with_callback(btn_b, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     gpio_set_irq_enabled_with_callback(btn_j, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
+    //Inicializando o display SSD1306
     i2c_init(I2C_PORT, 400 * 1000);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
@@ -62,17 +64,22 @@ int main()
     ssd1306_fill(&ssd, false);
     ssd1306_send_data(&ssd);
 
-    char ponto = 'A';
+    //Declaração de variáveis para uso dentro do loop
+    char ponto = 'A';//O caractere "A" está configurando para aparecer um ponto 8X8
     uint16_t valor_adc_X, valor_adc_Y;
     uint16_t posicao_X,posicao_Y;
 
+    //Loop principal
     while(true){
+        //Adquirindo os valores dos joystick usando o conversor ADC
         adc_select_input(0);
         valor_adc_X = adc_read();
         adc_select_input(1);
         valor_adc_Y = adc_read();
 
+        //Esse if serve para ativa ou desativar os led RED e BLUE
         if(estado_trava_led == false){
+            //Esses if servem para evitar que o led ligue caso está na zona morta
             if(valor_adc_X == 2047){
                 pwm_set_gpio_level(led_red_pino, 0);
             }
@@ -88,11 +95,14 @@ int main()
             }
         }
 
+        //Liga ou desligar o led GREEN
         gpio_put(led_green_pino, estado_green);
 
+        //Convertendo os valores do joystick para valores para serem usando como parâmetros de posicão no display
         posicao_X = valor_adc_X / 36;
         posicao_Y = valor_adc_Y / 82;
 
+        //Exibir o ponto na posição em que está o josytick
         ssd1306_fill(&ssd, !cor);
         ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor);
         ssd1306_draw_char(&ssd, ponto, posicao_X, posicao_Y);
@@ -102,11 +112,14 @@ int main()
     }
 }
 
+//Função para inicializar os pinos
 void iniciar_pinos(){
+    //Iniciando o led green
     gpio_init(led_green_pino);
     gpio_set_dir(led_green_pino, GPIO_OUT);
     gpio_put(led_green_pino, estado_green);
 
+    //Ininiciazando os led red e green como PWM
     gpio_set_function(led_red_pino, GPIO_FUNC_PWM);
     gpio_set_function(led_blue_pino, GPIO_FUNC_PWM);
     uint slice_red, slice_blue;
@@ -121,6 +134,7 @@ void iniciar_pinos(){
     pwm_set_enabled(slice_red, true);
     pwm_set_enabled(slice_blue, true);
 
+    //Inicializando os botões da placa
     gpio_init(btn_a);
     gpio_init(btn_b);
     gpio_init(btn_j);
@@ -131,11 +145,13 @@ void iniciar_pinos(){
     gpio_pull_up(btn_b);
     gpio_pull_up(btn_j);
 
+    //Inicializando os eixos do joystick usando o ADC
     adc_init();
     adc_gpio_init(joystick_pino_X);
     adc_gpio_init(joystick_pino_Y);
 }
 
+//Função para tratamento de interrupção
 void gpio_irq_handler(uint gpio, uint32_t events){
     uint32_t tempo_atual = to_ms_since_boot(get_absolute_time());
     if(tempo_atual - ultimo_tempo > 200){
